d552a00a6d153b4cac645a1799b7fc1e2e320c4a
danielxf007 <daniel.higuita007@gmail.com>
2022-08-11 11:36:00 -0500

    added new operations

--- core/scripting/assembler.py
+++ core/scripting/assembler.py
@@ -1,28 +1,6 @@
 import re
 
-class ByteLimits:
-    U_INT = 8
-
-class Language:
-    OP_0 = "OP_0"
-    OP_FALSE = "OP_FALSE"
-    DECIMAL_LEADING_ZEROES = "^0[0-9]+"
-    DECIMAL = "[0-9]|[1-9][0-9]*"
-    HEXADECIMAL = "0X([0-9]|[A-F])+"
-    OP_1NEGATE = "OP_1NEGATE"
-    OP_1 = "OP_1"
-    OP_TRUE = "OP_TRUE"
-    OP_2_TO_16 = "OP_([2-9]|1[0-6])"
-    OP_ADD = "OP_ADD"
-    OP_NUMEQUAL = "OP_NUMEQUAL"
-
-class ErrorMssg:
-    EMPTY_FILE = "The given file is empty"
-    UNDEFINED_INSTR = "Undefined Instruction"
-    LEADING_ZEROES = "Decimal numbers cannot have leading zeroes"
-    HEXADECIMAL = "0x must be followeb by hexadecimal numbers"
-    MAX_INT_BYTES = "The maximun number of bytes for an integer is 8"
-    COULD_NOT_ASSEMBLE = "Something went wrong and the file could not be assembled"
+from core.scripting.definitions import ByteSize, Language, AssemblerErrorMssg
 
 class Assembler:
     
@@ -35,25 +13,48 @@ class Assembler:
     def encode_push_0(self):
         op_code = (0x00).to_bytes(length=1, byteorder='little', signed=False)
         return op_code
-
-    def is_decimal_constant(self, string):
-        if re.fullmatch(Language.DECIMAL_LEADING_ZEROES, string):
-            raise Exception(f"{ErrorMssg.LEADING_ZEROES}: {string}")
-        return bool(re.fullmatch(Language.DECIMAL, string))
-
-    def is_hexadecimal_constant(self, string):
-        return bool(re.fullmatch(Language.HEXADECIMAL, string))
     
-    def encode_push_u_int_constant(self, string, base_number):
+    def is_decimal_integer(self, string):
+        if re.fullmatch(Language.DECIMAL_LEADING_ZEROES, string):
+            raise Exception(f"{AssemblerErrorMssg.LEADING_ZEROES}: {string}")
+        return bool(re.fullmatch(Language.DECIMAL_INTEGER, string))
+
+    def is_hexadecimal_integer(self, string):
+        return bool(re.fullmatch(Language.HEXADECIMAL_INTEGER, string))
+    
+    def encode_push_integer(self, string, base_number):
         number = int(string, base=base_number)
         n_bytes = (number.bit_length() + 7) // 8
-        if n_bytes <= ByteLimits.U_INT:
-            byte_arr = number.to_bytes(length=n_bytes, byteorder='little',signed=False)
-            op_code = n_bytes.to_bytes(length=1, byteorder='little', signed=False)
+        if n_bytes <= ByteSize.INT:
+            byte_arr = number.to_bytes(length=ByteSize.INT, byteorder='little',signed=True)
+            op_code = (ByteSize.INT).to_bytes(length=1, byteorder='little', signed=False)
             op_code += byte_arr
         else:
-            raise Exception(f"{ErrorMssg.MAX_INT_BYTES}: {string}")
-        return op_code  
+            raise Exception(f"{AssemblerErrorMssg.MAX_INT_BYTES}: {string}")
+        return op_code
+    
+    def is_string(self, string):
+        return bool(re.fullmatch(Language.STRING, string))
+    
+    def encode_push_string(self, string):
+        byte_arr = bytes(string, "ascii")
+        n_bytes = len(byte_arr)
+        if n_bytes <= ByteSize.STRING:
+            if n_bytes <= ByteSize.OP_1_75:
+                op_code = n_bytes.to_bytes(length=1, byteorder='little', signed=False)
+            elif n_bytes <= ByteSize.OP_PUSHDATA1:
+                op_code = (0x4c).to_bytes(length=1, byteorder='little', signed=False)
+                op_code += (n_bytes).to_bytes(length=1, byteorder='little', signed=False)
+            elif n_bytes <= ByteSize.OP_PUSHDATA2:
+                op_code = (0x4d).to_bytes(length=1, byteorder='little', signed=False)
+                op_code += (n_bytes).to_bytes(length=2, byteorder='little', signed=False)
+            elif n_bytes <= ByteSize.OP_PUSHDATA4:
+                op_code = (0x4d).to_bytes(length=1, byteorder='little', signed=False)
+                op_code += (n_bytes).to_bytes(length=4, byteorder='little', signed=False)
+            op_code += byte_arr
+        else:
+            raise Exception(AssemblerErrorMssg.MAX_BYTES)
+        return op_code
     
     def is_op1_negate(self, string):
         return Language.OP_1NEGATE == string
@@ -78,6 +79,13 @@ class Assembler:
     def enconde_push_2_to_16(self, string):
         offset = int(string.split("_")[1]) - 2
         op_code = (0x52 + offset).to_bytes(length=1, byteorder='little', signed=False)
+        return op_code
+    
+    def is_op_dup(self, string):
+        return Language.OP_DUP == string
+    
+    def encode_dup(self):
+        op_code = (0x76).to_bytes(length=1, byteorder='little', signed=False)
         return op_code
     
     def is_op_add(self, string):
@@ -120,10 +128,12 @@ class Assembler:
                 encoded_instr = self.encode_push_0()
             elif self.is_op_false(string):
                 encoded_instr = self.encode_push_0()
-            elif self.is_decimal_constant(string):
-                encoded_instr = self.encode_push_u_int_constant(string, 10)
-            elif self.is_hexadecimal_constant(string):
-                encoded_instr = self.encode_push_u_int_constant(string, 16)
+            elif self.is_decimal_integer(string):
+                encoded_instr = self.encode_push_integer(string, 10)
+            elif self.is_hexadecimal_integer(string):
+                encoded_instr = self.encode_push_integer(string, 16)
+            elif self.is_string(string):
+                encoded_instr = self.encode_push_string(string)
             elif self.is_op1_negate(string):
                 encoded_instr = self.encode_push_1_negate()
             elif self.is_op_1(string):
@@ -137,7 +147,7 @@ class Assembler:
             elif self.is_op_numequal(string):
                 encoded_instr = self.encode_numequal()
             else:
-                raise Exception(f"{ErrorMssg.UNDEFINED_INSTR}: {string}")
+                raise Exception(f"{AssemblerErrorMssg.UNDEFINED_INSTR}: {string}")
             encoded_instrs += encoded_instr
         return encoded_instrs
     
@@ -157,14 +167,14 @@ class Assembler:
         source_file_content -- it is the content of an assembly file which was opened
         with flag r
         """
-        result = {"success": True, "err": "", "encoded_file_content": bytes()}
+        result = {"success": True, "err": "", "binary": bytes()}
         try:
-            encoded_file_content = bytes("BTC".encode())
-            encoded_file_content += self.parse(source_file_content)
-            result["encoded_file_content"] = encoded_file_content
+            binary = bytes("BTC".encode())
+            binary += self.parse(source_file_content)
+            result["binary"] = binary
         except Exception as e:
             result["success"] = False
-            result["err"] = f"{ErrorMssg.COULD_NOT_ASSEMBLE}: {e}"
+            result["err"] = f"{AssemblerErrorMssg.COULD_NOT_ASSEMBLE}: {e}"
         return result
         
 
