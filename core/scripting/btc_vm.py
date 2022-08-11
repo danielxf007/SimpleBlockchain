ca9cf940059d434d7201985434edf142e5f26545
danielxf007 <daniel.higuita007@gmail.com>
2022-08-11 11:36:50 -0500

    integers are now 4 bytes

--- core/scripting/btc_vm.py
+++ core/scripting/btc_vm.py
@@ -1,3 +1,6 @@
+from core.scripting.definitions import ByteSize,BTCVMErrorMssg
+    
+
 class BTCVM:
     """ This class represents a stack based virtual machine that
     is able to execute functions according to the data in tokens
@@ -8,33 +11,48 @@ class BTCVM:
     """
     
     def __init__(self):
-        self.MAX_OP_CODES = 185
+        self.binary_type = bytes("BTC".encode())
+        self.MAX_OP_CODES = 256
+        self.function_vector = []
         self.stack = []
-        self.function_vector = [None for _ in range(self.MAX_OP_CODES)]
-        self.token_rom = []
+        self.program_rom = bytes()
         self.pc = 0 #Program counter
+        self.init_function_vector()
     
-    def init_function_map(self):
-        self.function_vector[0] = self.OP_0
-        for i in range(1, 76):
-            self.function_vector[i] = self.OP_1_75
-        self.function_vector[76] = self.OP_PUSHDATA1
-        self.function_vector[77] = self.OP_PUSHDATA2
-        self.function_vector[78] = self.OP_PUSHDATA4
-        self.function_vector[79] = self.OP_1NEGATE
-        self.function_vector[80] = self.OP_1
-        self.function_vector[139] = self.OP_1ADD
+    def init_function_vector(self):
+        self.function_vector = [None for _ in range(self.MAX_OP_CODES)]
+        self.function_vector[0x00] = self.push_empty_byte_arr
+        for i in range(0x01, 0x4c):
+            self.function_vector[i] = self.generate_push_data(i)
+        self.function_vector[0x4c] = None
+        self.function_vector[0x4d] = None
+        self.function_vector[0x4e] = None
+        self.function_vector[0x4f] = self.push_1_negate
+        self.function_vector[0x51] = self.push_1
+        for i in range(0x52, 0x61):
+            self.function_vector[i] = self.generate_op_2_16(i-0x50)
+        self.function_vector[0x93] = None
+        self.function_vector[0x9c] = None
+    
+    def reset(self):
+        self.stack.clear()
+        self.program_rom = bytes()
+        self.pc = 0
+    
+    def get_stack(self):
+        return self.stack
     
     def push(self, byte_arr):
         self.stack.append(byte_arr)
     
     def pop(self):
-        pass
+        return self.stack.pop()
 
-    def OP_0(self):
+    def push_empty_byte_arr(self):
         self.push(bytes())
+        self.pc += 1
 
-    def OP_1_75(self, value):
+    def generate_push_data(self, n_bytes):
         """Push a byte array of a given size onto the stack.
 
         The byte array is little endian.
@@ -43,61 +61,33 @@ class BTCVM:
         value -- it is the number of bytes that will be pushed, its minimun is 1 and
         its maximun is 75 
         """
-        self.push(bytes(value))
-        """value.to_bytes(length=value.bit_length() + 7) // 8,
-         byteorder="little", signed=True
-        """
+        def push_n_bytes():
+            if self.pc+n_bytes > len(self.program_rom)-1:
+                raise Exception(BTCVMErrorMssg.NOT_ENOUGH_BYTES)
+            byte_arr = self.program_rom[self.pc+1 : (self.pc+1)+n_bytes]
+            self.push(byte_arr)
+            self.pc += (n_bytes + 1)
+        return push_n_bytes
     
-    def OP_PUSHDATA1(self, value):
-        """Takes the next 1 byte as the number of bytes to push onto the stack.
-
-        The byte array is little endian.
-
-        Keyword arguments:
-        value -- it is the number of bytes that will be pushed, its minimun is 1 and
-        its maximun is 255 
-        """
-        self.push(bytes(value))
-    
-    def OP_PUSHDATA2(self, value):
-        """Takes the next 2 bytes as the number of bytes to push onto the stack.
-
-        The byte array is little endian.
-
-        Keyword arguments:
-        value -- it is the number of bytes that will be pushed, its minimun is 256 and
-        its maximun is 65,535
-        """
-        self.push(bytes(value))
-    
-    def OP_PUSHDATA4(self, value):
-        """Takes the next 2 bytes as the number of bytes to push onto the stack.
-
-        The byte array is little endian.
-
-        Keyword arguments:
-        value -- it is the number of bytes that will be pushed, its minimun is 65,536 and
-        its maximun is 4,294,967,295
-        """
-        self.push(bytes(value))
-    
-    def OP_1NEGATE(self):
+    def push_1_negate(self):
         """Pushes the number -1 as an array of bytes of length 1 onto the stack.
         
         The byte array is little endian.
         """
         value = -1
-        self.push(value.to_bytes(length=1, byteorder="little", signed=True))
+        self.push(value.to_bytes(length=ByteSize.INT, byteorder="little", signed=True))
+        self.pc += 1
     
-    def OP_1(self):
+    def push_1(self):
         """Pushes the number 1 as an array of bytes of length 1 onto the stack.
         
         The byte array is little endian.
         """
         value = 1
-        self.push(value.to_bytes(length=1, byteorder="little", signed=True))
+        self.push(value.to_bytes(length=ByteSize.INT, byteorder="little", signed=True))
+        self.pc += 1
     
-    def OP_2_16(self, value):
+    def generate_op_2_16(self, n):
         """Pushes a value as a byte array of length 1 onto the stack.
 
         The byte array is little endian.
@@ -105,10 +95,58 @@ class BTCVM:
         Keyword arguments:
         value -- it is the number that will be pushed
         """
-        self.push(value.to_bytes(length=1, byteorder="little", signed=True))
+        def push_n():
+            self.push(n.to_bytes(length=ByteSize.INT, byteorder="little", signed=False))
+            self.pc += 1
+        return push_n
 
-    def OP_1ADD(self, args):
-        pass
+    def add(self):
+        if len(self.stack) < 2:
+            raise Exception(BTCVMErrorMssg.BINARY_OP)
+        value_1 = int.from_bytes(bytes=self.pop(), byteorder="little", signed=True)
+        value_2 = int.from_bytes(bytes=self.pop(), byteorder="little", signed=True)
+        number = value_1 + value_2
+        n_bytes = (number.bit_length() + 7) // 8
+        if n_bytes <= ByteSize.INT:
+            self.push(number.to_bytes(length=ByteSize.INT, byteorder="little", signed=True))
+        else:
+            self.push(number.to_bytes(length=n_bytes, byteorder="little", signed=True))
+        self.pc += 1
+    
+    def numequal(self):
+        if len(self.stack) < 2:
+            raise Exception(BTCVMErrorMssg.BINARY_OP)
+        value_1 = int.from_bytes(bytes=self.pop(), byteorder="little", signed=True)
+        value_2 = int.from_bytes(bytes=self.pop(), byteorder="little", signed=True)
+        if value_1 == value_2:
+            self.push((1).to_bytes(length=ByteSize.INT, byteorder="little", signed=True))
+        else:
+            self.push((0).to_bytes(length=ByteSize.INT, byteorder="little", signed=True))
+        self.pc += 1
+
+    def is_btc_binary(self, binary_type):
+        return self.binary_type == binary_type
+    
+    def load_program(self, binary_program):
+        self.program_rom += binary_program
+
+    def process(self, binary):
+        result = {"success": True, "err": ""}
+        if len(binary) < 3 or not self.is_btc_binary(binary[0:3]):
+            result["success"] = False
+            result["err"] = BTCVMErrorMssg.NOT_BTC
+            return result
+        self.load_program(binary[3:])
+        try:
+            while self.pc < len(self.program_rom):
+                op_code = self.program_rom[self.pc]
+                func = self.function_vector[op_code]
+                func()
+            result["success"] = True
+        except Exception as e:
+            result["success"] = False
+            result["err"] = f"{BTCVMErrorMssg.COULD_NOT_PROCESS}: {e}"
+        return result
 
 
     
