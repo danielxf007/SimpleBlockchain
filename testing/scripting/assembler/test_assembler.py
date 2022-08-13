3cbd9789ea314d0c7471c2a04e1f8140bf78c1d4
danielxf007 <daniel.higuita007@gmail.com>
2022-08-12 22:47:46 -0500

    testing changes made with parser

--- testing/scripting/assembler/test_assembler.py
+++ testing/scripting/assembler/test_assembler.py
@@ -21,3 +21,12 @@ class TestAssembler:
             target_file.write(result["binary"])
         assert result["success"] == True
         assert filecmp.cmp(target_path, expected_path) == True
+assembler = Assembler()
+source_path = "./test_files/input/constants.s"
+target_path = "./test_files/output/constants.btc"
+expected_path = "./test_files/output/constants.btc"
+result = {}
+with open(source_path, 'r') as source_file, open(target_path, 'wb') as target_file:
+    source_file_content = source_file.read()
+    result = assembler.assemble(source_file_content)
+    print(result)
