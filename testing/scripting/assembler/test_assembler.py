70bf47eef05d199459a18ca8d2177ddbdb34c8c6
danielxf007 <daniel.higuita007@gmail.com>
2022-08-15 16:22:04 -0500

    added new test for stack, crypto and bitwise operations

--- testing/scripting/assembler/test_assembler.py
+++ testing/scripting/assembler/test_assembler.py
@@ -25,11 +25,43 @@ class TestAssembler:
         result = self.assemble(source_path, target_path)
         assert result["success"] == True
         assert filecmp.cmp(target_path, expected_path) == True
+
+    def test_flow_control(self):
+        source_path = "./test_files/input/flow_control.s"
+        target_path = "./test_files/output/flow_control.btc"
+        expected_path = "./test_files/output/flow_control_expected.btc"
+        result = self.assemble(source_path, target_path)
+        assert result["success"] == True
+        assert filecmp.cmp(target_path, expected_path) == True
+
+    def test_stack(self):
+        source_path = "./test_files/input/stack.s"
+        target_path = "./test_files/output/stack.btc"
+        expected_path = "./test_files/output/stack_expected.btc"
+        result = self.assemble(source_path, target_path)
+        assert result["success"] == True
+        assert filecmp.cmp(target_path, expected_path) == True
+    
+    def test_bitwise_logic(self):
+        source_path = "./test_files/input/bitwise_logic.s"
+        target_path = "./test_files/output/bitwise_logic.btc"
+        expected_path = "./test_files/output/bitwise_logic_expected.btc"
+        result = self.assemble(source_path, target_path)
+        assert result["success"] == True
+        assert filecmp.cmp(target_path, expected_path) == True
     
     def test_arithmetic(self):
         source_path = "./test_files/input/arithmetic.s"
         target_path = "./test_files/output/arithmetic.btc"
         expected_path = "./test_files/output/arithmetic_expected.btc"
+        result = self.assemble(source_path, target_path)
+        assert result["success"] == True
+        assert filecmp.cmp(target_path, expected_path) == True
+
+    def test_cryptography(self):
+        source_path = "./test_files/input/cryptography.s"
+        target_path = "./test_files/output/cryptography.btc"
+        expected_path = "./test_files/output/cryptography_expected.btc"
         result = self.assemble(source_path, target_path)
         assert result["success"] == True
         assert filecmp.cmp(target_path, expected_path) == True
