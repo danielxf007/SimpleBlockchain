52949f8eebe7194213ac724e6108c34df874d0ed
danielxf007 <daniel.higuita007@gmail.com>
2022-08-15 00:12:15 -0500

    added a new test for arithmetic operations

--- testing/scripting/assembler/test_assembler.py
+++ testing/scripting/assembler/test_assembler.py
@@ -9,25 +9,27 @@ from core.scripting.assembler import Assembler
 class TestAssembler:
 
     assembler = Assembler()
-    
-    def test_push_constants(self):
-        source_path = "./test_files/input/constants.s"
-        target_path = "./test_files/output/constants.btc"
-        expected_path = "./test_files/output/constants_expected.btc"
+
+    def assemble(self, source_path, target_path):
         result = {}
         with open(source_path, 'r') as source_file, open(target_path, 'wb') as target_file:
             source_file_content = source_file.read()
             result = self.assembler.assemble(source_file_content)
             target_file.write(result["binary"])
+        return result
+    
+    def test_push_constants(self):
+        source_path = "./test_files/input/constants.s"
+        target_path = "./test_files/output/constants.btc"
+        expected_path = "./test_files/output/constants_expected.btc"
+        result = self.assemble(source_path, target_path)
         assert result["success"] == True
         assert filecmp.cmp(target_path, expected_path) == True
-
-"""assembler = Assembler()
-source_path = "./test_files/input/constants.s"
-target_path = "./test_files/output/constants.btc"
-expected_path = "./test_files/output/constants.btc"
-result = {}
-with open(source_path, 'r') as source_file, open(target_path, 'wb') as target_file:
-    source_file_content = source_file.read()
-    result = assembler.assemble(source_file_content)
-    print(result)"""
+    
+    def test_arithmetic(self):
+        source_path = "./test_files/input/arithmetic.s"
+        target_path = "./test_files/output/arithmetic.btc"
+        expected_path = "./test_files/output/arithmetic_expected.btc"
+        result = self.assemble(source_path, target_path)
+        assert result["success"] == True
+        assert filecmp.cmp(target_path, expected_path) == True
