a53695a2df70c30cad0bdfabd62f2378f4b57244
danielxf007 <daniel.higuita007@gmail.com>
2022-08-21 22:04:49 -0500

    added a unlock script

--- core/transactions/tx_in.py
+++ core/transactions/tx_in.py
@@ -1,7 +1,7 @@
 class TXIn:
-    """This class represents a transaction input"""
+    """This data structure represents a transaction input"""
 
-    def __init__(self, tx_hash, utxo_index, public_key):
+    def __init__(self, tx_hash, utxo_index, unlock_script):
         """Initializes the class' attributes
         
         Keyword arguments:
@@ -9,9 +9,9 @@ class TXIn:
         unspent transaction output that will be spend
         utxo_index -- it indicates which unspent transaction output created in a tx
         will be spent
-        public_key -- it is the public key of the one
-        that will spend the unspent transaction output
+        unlock_script -- it is a script with push only operations, which solved the
+        lock script of the referenced utxo
         """
         self.tx_hash = tx_hash
         self.utxo_index = utxo_index
-        self.public_key = public_key
+        self.unlock_script = unlock_script
