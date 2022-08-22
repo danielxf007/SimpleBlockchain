4f63c1053d6fd58cb90bcb063ac513671e400d95
danielxf007 <daniel.higuita007@gmail.com>
2022-08-21 22:08:44 -0500

    added comments to test the new comment symbol

--- testing/scripting/assembler/test_files/input/arithmetic.s
+++ testing/scripting/assembler/test_files/input/arithmetic.s
@@ -1,3 +1,9 @@
-15 15 OP_ADD 30 OP_NUMEQUAL
+#This test is used to test arithmetic operations
 
--10 4 op_add -6 op_numequal
+# START
+
+15 15 OP_ADD 30 OP_NUMEQUAL # checks if 15 + 15 = 30
+
+-10 4 op_add -6 op_numequal # checks if -10 + 4 = 6
+
+# END
