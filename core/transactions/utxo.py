efefd5288518deed9ed832e70ce942fdd1184b53
danielxf007 <daniel.higuita007@gmail.com>
2022-08-21 22:05:23 -0500

    added a lock script

--- core/transactions/utxo.py
+++ core/transactions/utxo.py
@@ -1,24 +1,13 @@
 class UTXO:
-    """This class represents a unspent transaction output"""
+    """This data structure represents a unspent transaction output"""
 
-    def __init__(self, value, public_key):
+    def __init__(self, value, lock_script):
         """Initializes the class' attributes
 
         Keyword arguments:
         value -- it is the amount of satoshi this unspent transaction output is worth
-        public_key -- it is the public key that can spend this unspent transaction output
+        lock_script -- it is the script key that if given the correct set of inputs
+        allows the utxo to be spent
         """
         self.value = value
-        self.public_key = public_key
-    
-    def can_spend(self, public_key):
-        """Checks whether a public key can spend this unspent transaction output or not
-
-        Keyword arguments:
-        public_key -- it is the public key that wants to spend this unspent transaction output
-        """
-        return self.public_key == public_key
-    
-    def get_value(self):
-        """Returns the value of this unspent transaction output"""
-        return self.value
+        self.lock_script = lock_script
