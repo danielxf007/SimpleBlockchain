610d93f29900b85cd5151e51b005f5e565ebe4d7
danielxf007 <daniel.higuita007@gmail.com>
2022-08-21 22:00:33 -0500

    deledted unused funcs

--- core/data_bases/utxo_reference_db.py
+++ core/data_bases/utxo_reference_db.py
@@ -4,14 +4,23 @@ class UTXOReferenceDB:
     unspent transaction output
     """
 
-    def __init__(self):
+    def __init__(self, references):
         """Initializes the class' attributes
 
-        The class uses an array to store the metadata
-        """
-        self.utxo_references = []
+        Keyword arguments:
 
-    def add_utxo_reference(self, tx_hash, utxo_index):
+        references -- it is a copy of the global state of references
+        """
+        self.references = references
+    
+    def set_references(self, references):
+        self.references = references
+
+    def get_references(self):
+        """Retrieves the whole array of references"""
+        return self.references
+
+    def add_reference(self, tx_hash, utxo_index):
         """Stores the transaction hash and the index of the unspent transaction output
         into the database
         
@@ -21,26 +30,11 @@ class UTXOReferenceDB:
         utxo_index -- it is an index that references a unspent transaction output contained in
         the unspent transaction output array
         """        
-        self.utxo_references.append({
+        self.references.append({
             "tx_hash": tx_hash,
             "utxo_index": utxo_index})
-    
-    def add_utxo_references(self, tx_hash, utxo_arr):
-        """Stores the transaction hash and the index of the unspent transaction output
-        into the database
-        
-        Keyword arguments:
-        tx_hash -- it is the hash of the transaction that contains
-        the unspent transation output array
-        utxo_arr -- it is an array of unspent transaction outputs,
-        it contains two elements, the first one is a unspent transaction output
-        with the change and the second one is a new unspent transaction output
-        which a beneficiary can spend 
-        """
-        for utxo_index in range(len(utxo_arr)):
-            self.add_utxo_reference(tx_hash, utxo_index)
 
-    def remove_utxo_reference(self, tx_hash, utxo_index):
+    def remove_reference(self, tx_hash, utxo_index):
         """Removes the reference that has the given trasaction hash and
         unspent transaction output index from the database
         
@@ -50,23 +44,14 @@ class UTXOReferenceDB:
         utxo_index -- it is an index that references a unspent transaction output contained in
         the unspent transaction output array
         """  
-        for utxo_reference in self.utxo_references:
-            if (utxo_reference["tx_hash"] == tx_hash and
-                utxo_reference["utxo_index"] == utxo_index):
-                self.utxo_references.remove(utxo_reference)
+        for reference in self.references:
+            if reference["tx_hash"] == tx_hash and reference["utxo_index"] == utxo_index:
+                self.references.remove(reference)
                 break
-    
-    def remove_utxos_reference(self, tx_in_arr):
-        """Removes the references to unspent transaction outputs contained in
-        the trasaction input array 
         
-        Keyword arguments:
-        tx_in_arr -- it contains the transaction inputs that reference the 
-        unspent transaction outputs
-        """  
-        for tx_in in tx_in_arr:
-            self.remove_utxo_reference(tx_in.tx_hash, tx_in.utxo_index)
+    def has_reference(self, tx_hash, utxo_index):
+        for reference in self.references:
+            if reference["tx_hash"] == tx_hash and reference["utxo_index"] == utxo_index:
+                return True
+        return False
     
-    def get_utxo_references(self):
-        """Retrieves the whole array of references"""
-        return self.utxo_references
